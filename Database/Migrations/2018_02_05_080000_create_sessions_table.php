0e02f964c6b63e3ae6dd2873a30b96797478be24
marco76tv <marco.sottana@gmail.com>
2019-08-28 12:14:15 +0200

    .

--- Database/Migrations/2018_02_05_080000_create_sessions_table.php
+++ Database/Migrations/2018_02_05_080000_create_sessions_table.php
@@ -1,22 +1,25 @@
 <?php
-
-
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+//----- models -----
+use Modules\Extend\Models\Session as MyModel;
+
+
 class CreateSessionsTable extends Migration
 {
-    protected $table = 'sessions'; //name of pack +"_"+ name of table, but this is a standard table
-
+    //protected $table = 'sessions'; //name of pack +"_"+ name of table, but this is a standard table
+    public function getTable(){
+        return with(new MyModel())->getTable();
+    }
     /**
      * Run the migrations.
      */
     public function up()
     {
-        if (!Schema::hasTable($this->table)) {
-            Schema::create($this->table, function (Blueprint $table) {
+        if (!Schema::hasTable($this->getTable())) {
+            Schema::create($this->getTable(), function (Blueprint $table) {
                 $table->string('id', 191)->unique();
                 $table->unsignedInteger('user_id')->nullable();
                 $table->string('ip_address', 45)->nullable();
@@ -32,8 +35,8 @@ class CreateSessionsTable extends Migration
      */
     public function down()
     {
-        if (Schema::hasTable($this->table)) {
-            Schema::dropIfExists($this->table);
+        if (Schema::hasTable($this->getTable())) {
+            Schema::dropIfExists($this->getTable());
         }
     }
 }
--- Database/Migrations/2018_02_07_000000_create_cache_table.php
+++ Database/Migrations/2018_02_07_000000_create_cache_table.php
@@ -1,22 +1,25 @@
 <?php
-
-
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+//----- models -----
+use Modules\Extend\Models\Cache as MyModel;
+
+
 class CreateCacheTable extends Migration
 {
-    protected $table = 'cache'; //name of pack +"_"+ name of table, but this is a standard table
-
+    //protected $table = 'cache'; //name of pack +"_"+ name of table, but this is a standard table
+    public function getTable(){
+        return with(new MyModel())->getTable();
+    }
     /**
      * Run the migrations.
      */
     public function up()
     {
-        if (!Schema::hasTable($this->table)) {
-            Schema::create($this->table, function (Blueprint $table) {
+        if (!Schema::hasTable($this->getTable())) {
+            Schema::create($this->getTable(), function (Blueprint $table) {
                 $table->string('key', 64)->unique();
                 $table->text('value');
                 $table->integer('expiration');
@@ -29,6 +32,6 @@ class CreateCacheTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists($this->table);
+        Schema::dropIfExists($this->getTable());
     }
 }
